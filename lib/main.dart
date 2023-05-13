7c83100005aa0338cc81ee7482ffeb083f9455e2
सौरभ पाटील <mr.saurhub@gmail.com>
2023-05-13 16:31:30 +0530

    MyMediaApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'utils/routes/route_name.dart';
+import 'utils/routes/routes.dart';
+import 'utils/theme.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.themeData,
+      initialRoute: RouteName.loginScreen,
+      onGenerateRoute: Routes.generateRoute,
     );
   }
 }
